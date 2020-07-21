0686a3e0e915df836d0517c4eedbca8114aa8dd4
iagox86 <ron-git@skullsecurity.org>
2020-07-21 15:58:15 -0700

    Add other xor transformations

--- src/lib.rs
+++ src/lib.rs
@@ -116,8 +116,23 @@ impl Transformation {
         true
     }
 
-    fn transform_xor16(mut _buffer: Vec<u8>, _c: u16) -> SimpleResult<Vec<u8>> {
-        bail!("Not implemented");
+    fn transform_xor16(mut buffer: Vec<u8>, c: u16) -> SimpleResult<Vec<u8>> {
+        if !Self::check_xor16(&buffer, c) {
+            bail!("Buffer isn't a multiple of 16 bits");
+        }
+
+        let xorer: Vec<u8> = vec![
+            ((c >> 8) & 0x00FF) as u8,
+            ((c >> 0) & 0x00FF) as u8,
+        ];
+
+        let mut xor_position: usize = 0;
+        for n in &mut buffer {
+            *n = *n ^ (xorer[xor_position]);
+            xor_position = (xor_position + 1) % 2;
+        }
+
+        Ok(buffer)
     }
 
     fn untransform_xor16(buffer: Vec<u8>, c: u16) -> SimpleResult<Vec<u8>> {
@@ -128,8 +143,25 @@ impl Transformation {
         (buffer.len() % 2) == 0
     }
 
-    fn transform_xor32(mut _buffer: Vec<u8>, _c: u32) -> SimpleResult<Vec<u8>> {
-        bail!("Not implemented");
+    fn transform_xor32(mut buffer: Vec<u8>, c: u32) -> SimpleResult<Vec<u8>> {
+        if !Self::check_xor32(&buffer, c) {
+            bail!("Buffer isn't a multiple of 32 bits");
+        }
+
+        let xorer: Vec<u8> = vec![
+            ((c >> 24) & 0x00FF) as u8,
+            ((c >> 16) & 0x00FF) as u8,
+            ((c >> 8)  & 0x00FF) as u8,
+            ((c >> 0)  & 0x00FF) as u8,
+        ];
+
+        let mut xor_position: usize = 0;
+        for n in &mut buffer {
+            *n = *n ^ (xorer[xor_position]);
+            xor_position = (xor_position + 1) % 4;
+        }
+
+        Ok(buffer)
     }
 
     fn untransform_xor32(buffer: Vec<u8>, c: u32) -> SimpleResult<Vec<u8>> {
@@ -140,8 +172,29 @@ impl Transformation {
         (buffer.len() % 4) == 0
     }
 
-    fn transform_xor64(mut _buffer: Vec<u8>, _c: u64) -> SimpleResult<Vec<u8>> {
-        bail!("Not implemented");
+    fn transform_xor64(mut buffer: Vec<u8>, c: u64) -> SimpleResult<Vec<u8>> {
+        if !Self::check_xor64(&buffer, c) {
+            bail!("Buffer isn't a multiple of 32 bits");
+        }
+
+        let xorer: Vec<u8> = vec![
+            ((c >> 56) & 0x00FF) as u8,
+            ((c >> 48) & 0x00FF) as u8,
+            ((c >> 40) & 0x00FF) as u8,
+            ((c >> 32) & 0x00FF) as u8,
+            ((c >> 24) & 0x00FF) as u8,
+            ((c >> 16) & 0x00FF) as u8,
+            ((c >> 8)  & 0x00FF) as u8,
+            ((c >> 0)  & 0x00FF) as u8,
+        ];
+
+        let mut xor_position: usize = 0;
+        for n in &mut buffer {
+            *n = *n ^ (xorer[xor_position]);
+            xor_position = (xor_position + 1) % 8;
+        }
+
+        Ok(buffer)
     }
 
     fn untransform_xor64(buffer: Vec<u8>, c: u64) -> SimpleResult<Vec<u8>> {
@@ -279,6 +332,11 @@ impl Transformation {
     }
 
     pub fn transform(&self, buffer: Vec<u8>) -> SimpleResult<Vec<u8>> {
+        // We can never handle 0-length buffers
+        if buffer.len() == 0 {
+            bail!("Cannot transform 0-length buffer");
+        }
+
         match self {
             Self::Null                               => Self::transform_null(buffer),
             Self::XorByConstant8(c)                  => Self::transform_xor8(buffer, *c),
@@ -307,6 +365,11 @@ impl Transformation {
     }
 
     pub fn untransform(&self, buffer: Vec<u8>) -> SimpleResult<Vec<u8>> {
+        // We can never handle 0-length buffers
+        if buffer.len() == 0 {
+            bail!("Cannot untransform 0-length buffer");
+        }
+
         match self {
             Self::Null                          => Self::untransform_null(buffer),
             Self::XorByConstant8(c)             => Self::untransform_xor8(buffer, *c),
@@ -335,6 +398,11 @@ impl Transformation {
     }
 
     pub fn can_transform(&self, buffer: &Vec<u8>) -> bool {
+        // We can never handle 0-length buffers
+        if buffer.len() == 0 {
+            return false;
+        }
+
         match self {
             Self::Null                               => Self::check_null(buffer),
             Self::XorByConstant8(c)                  => Self::check_xor8(buffer, *c),
@@ -403,7 +471,6 @@ mod tests {
         assert_eq!(true, Transformation::Null.can_untransform());
 
         let tests: Vec<(Vec<u8>, SimpleResult<Vec<u8>>)> = vec![
-            (vec![],              Ok(vec![])),
             (vec![1],             Ok(vec![1])),
             (vec![1, 2, 3],       Ok(vec![1, 2, 3])),
             (vec![1, 2, 3, 4, 5], Ok(vec![1, 2, 3, 4, 5])),
@@ -421,12 +488,10 @@ mod tests {
     }
 
     #[test]
-    fn test_xor_by_constant() -> SimpleResult<()> {
+    fn test_xor8() -> SimpleResult<()> {
         assert_eq!(true, Transformation::XorByConstant8(0).can_untransform());
 
         let tests: Vec<(u8, Vec<u8>, SimpleResult<Vec<u8>>)> = vec![
-            (0, vec![],              Ok(vec![])),
-
             (0, vec![1],             Ok(vec![1])),
             (0, vec![1, 2, 3],       Ok(vec![1, 2, 3])),
             (0, vec![1, 2, 3, 4, 5], Ok(vec![1, 2, 3, 4, 5])),
@@ -451,6 +516,146 @@ mod tests {
         Ok(())
     }
 
+    #[test]
+    fn test_xor16() -> SimpleResult<()> {
+        let t = Transformation::XorByConstant16(0x0000);
+
+        // It can transform even-length vectors
+        assert!(t.can_transform(&vec![0x11, 0x22]));
+        assert!(t.can_transform(&vec![0x11, 0x22, 0x33, 0x44]));
+
+        // It cannot transform odd-length vectors
+        assert!(!t.can_transform(&vec![0x11]));
+        assert!(!t.can_transform(&vec![0x11, 0x22, 0x33]));
+
+        // Simplest examples
+        let t = Transformation::XorByConstant16(0x0000);
+        assert_eq!(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66], t.transform(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66])?);
+
+        let t = Transformation::XorByConstant16(0xFFFF);
+        assert_eq!(vec![0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99], t.transform(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66])?);
+
+        // More complex examples
+        let t = Transformation::XorByConstant16(0x1234);
+
+        // First byte: 0x11 & 0x12 = 0x03
+        // Second byte: 0x22 & 0x34 = 0x16
+        assert_eq!(vec![0x03, 0x16], t.transform(vec![0x11, 0x22])?);
+
+        // Third byte: 0x33 & 0x12 = 0x21
+        // Fourth byte: 0x44 & 0x34 = 0x70
+        assert_eq!(vec![0x03, 0x16, 0x21, 0x70], t.transform(vec![0x11, 0x22, 0x33, 0x44])?);
+
+        // Fail on bad strings
+        assert!(t.transform(vec![0x11]).is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_xor32() -> SimpleResult<()> {
+        let t = Transformation::XorByConstant32(0x00000000);
+
+        // It can transform multiple-of-4 vectors
+        assert!(t.can_transform(&vec![0x11, 0x22, 0x33, 0x44]));
+        assert!(t.can_transform(&vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]));
+
+        // It cannot transform odd-length vectors
+        assert!(!t.can_transform(&vec![0x11]));
+        assert!(!t.can_transform(&vec![0x11, 0x33]));
+        assert!(!t.can_transform(&vec![0x11, 0x22, 0x33]));
+        assert!(!t.can_transform(&vec![0x11, 0x22, 0x33, 0x44, 0x55]));
+
+        // Simplest examples
+        let t = Transformation::XorByConstant32(0x00000000);
+        assert_eq!(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88], t.transform(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88])?);
+
+        let t = Transformation::XorByConstant32(0xFFFFFFFF);
+        assert_eq!(vec![0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88, 0x77], t.transform(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88])?);
+
+        // More complex examples
+        let t = Transformation::XorByConstant32(0x12345678);
+
+        // First byte:  0x11 & 0x12 = 0x03
+        // Second byte: 0x22 & 0x34 = 0x16
+        // Third byte:  0x33 & 0x56 = 0x65
+        // Fourth byte: 0x44 & 0x78 = 0x3c
+        assert_eq!(vec![0x03, 0x16, 0x65, 0x3c], t.transform(vec![0x11, 0x22, 0x33, 0x44])?);
+
+        // Fifth byte:   0x55 & 0x12 = 0x47
+        // Sixth byte:   0x66 & 0x34 = 0x52
+        // Seventh byte: 0x77 & 0x56 = 0x21
+        // Eighth byte:  0x88 & 0x78 = 0xf0
+        assert_eq!(vec![0x03, 0x16, 0x65, 0x3c, 0x47, 0x52, 0x21, 0xf0], t.transform(vec![0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88])?);
+
+        //assert_eq!(vec![0x03, 0x16, 0x21, 0x70], t.transform(vec![0x11, 0x22, 0x33, 0x44])?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_xor64() -> SimpleResult<()> {
+        let t = Transformation::XorByConstant64(0x0000000000000000);
+
+        // It can transform multiple-of-8 vectors
+        assert!(t.can_transform(&vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77]));
+        assert!(t.can_transform(&vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff]));
+
+        // It cannot transform anything else
+        assert!(!t.can_transform(&vec![0x00]));
+        assert!(!t.can_transform(&vec![0x00, 0x11]));
+        assert!(!t.can_transform(&vec![0x00, 0x11, 0x22]));
+        assert!(!t.can_transform(&vec![0x00, 0x11, 0x22, 0x33]));
+        assert!(!t.can_transform(&vec![0x00, 0x11, 0x22, 0x33, 0x44]));
+        assert!(!t.can_transform(&vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55]));
+        assert!(!t.can_transform(&vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66]));
+        assert!(!t.can_transform(&vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88]));
+
+        // Simplest examples
+        let t = Transformation::XorByConstant64(0x0000000000000000);
+        assert_eq!(
+            vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff],
+            t.transform(vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff])?
+        );
+
+        let t = Transformation::XorByConstant64(0xFFFFFFFFFFFFFFFF);
+        assert_eq!(
+            vec![0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00],
+            t.transform(vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff])?
+        );
+
+        // // More complex examples
+        let t = Transformation::XorByConstant64(0x0123456789abcdef);
+
+        // First byte:   0x00 & 0x01 = 0x01
+        // Second byte:  0x11 & 0x23 = 0x32
+        // Third byte:   0x22 & 0x45 = 0x67
+        // Fourth byte:  0x33 & 0x67 = 0x54
+        // Fifth byte:   0x44 & 0x89 = 0xcd
+        // Sixth byte:   0x55 & 0xab = 0xfe
+        // Seventh byte: 0x66 & 0xcd = 0xab
+        // Eighth byte:  0x77 & 0xef = 0x98
+        assert_eq!(
+            vec![0x01, 0x32, 0x67, 0x54, 0xcd, 0xfe, 0xab, 0x98],
+            t.transform(vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77])?
+        );
+
+        // First byte:   0x88 & 0x01 = 0x89
+        // Second byte:  0x99 & 0x23 = 0xba
+        // Third byte:   0xaa & 0x45 = 0xef
+        // Fourth byte:  0xbb & 0x67 = 0xdc
+        // Fifth byte:   0xcc & 0x89 = 0x45
+        // Sixth byte:   0xdd & 0xab = 0x76
+        // Seventh byte: 0xee & 0xcd = 0x23
+        // Eighth byte:  0xff & 0xef = 0x10
+        assert_eq!(
+           vec![0x01, 0x32, 0x67, 0x54, 0xcd, 0xfe, 0xab, 0x98, 0x89, 0xba, 0xef, 0xdc, 0x45, 0x76, 0x23, 0x10],
+            t.transform(vec![0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff])?
+        );
+
+        Ok(())
+    }
+
     // Just a small convenience function for tests
     fn b(s: &[u8]) -> Vec<u8> {
         Vec::from(s)
@@ -460,12 +665,6 @@ mod tests {
     fn test_base64_standard() -> SimpleResult<()> {
         let t = Transformation::FromBase64;
         assert_eq!(true, t.can_untransform());
-
-        // Empty string: ""
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-        let original = t.untransform(result)?;
-        assert_eq!(b(b""), original);
 
         // Short string: "\x00"
         let result = t.transform(b(b"AA=="))?;
@@ -502,12 +701,6 @@ mod tests {
         let t = Transformation::FromBase64NoPadding;
         assert_eq!(true, t.can_untransform());
 
-        // Empty string: ""
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-        let original = t.untransform(result)?;
-        assert_eq!(b(b""), original);
-
         // Short string: "\x00"
         let result = t.transform(b(b"AA"))?;
         assert_eq!(b(b"\x00"), result);
@@ -543,10 +736,6 @@ mod tests {
         let t = Transformation::FromBase64Permissive;
         assert_eq!(false, t.can_untransform());
 
-        // Empty string: ""
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-
         // Short string: "\x00" with various padding
         assert_eq!(b(b"\x00"), t.transform(b(b"AA"))?);
         assert_eq!(b(b"\x00"), t.transform(b(b"AA="))?);
@@ -562,12 +751,6 @@ mod tests {
     fn test_base64_url() -> SimpleResult<()> {
         let t = Transformation::FromBase64URL;
         assert_eq!(true, t.can_untransform());
-
-        // Empty string: ""
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-        let original = t.untransform(result)?;
-        assert_eq!(b(b""), original);
 
         // Short string: "\x00"
         let result = t.transform(b(b"AA=="))?;
@@ -604,12 +787,6 @@ mod tests {
         let t = Transformation::FromBase64URLNoPadding;
         assert_eq!(true, t.can_untransform());
 
-        // Empty string: ""
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-        let original = t.untransform(result)?;
-        assert_eq!(b(b""), original);
-
         // Short string: "\x00"
         let result = t.transform(b(b"AA"))?;
         assert_eq!(b(b"\x00"), result);
@@ -645,10 +822,6 @@ mod tests {
         let t = Transformation::FromBase64URLPermissive;
         assert_eq!(false, t.can_untransform());
 
-        // Empty string: ""
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-
         // Short string: "\x00" with various padding
         assert_eq!(b(b"\x00"), t.transform(b(b"AA"))?);
         assert_eq!(b(b"\x00"), t.transform(b(b"AA="))?);
@@ -663,13 +836,6 @@ mod tests {
     fn test_base32_standard() -> SimpleResult<()> {
         let t = Transformation::FromBase32;
         assert_eq!(true, t.can_untransform());
-
-        // Empty string: ""
-        let t = Transformation::FromBase32;
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-        let original = t.untransform(result)?;
-        assert_eq!(b(b""), original);
 
         // Short string: "\x00"
         let t = Transformation::FromBase32;
@@ -715,13 +881,6 @@ mod tests {
         let t = Transformation::FromBase32NoPadding;
         assert_eq!(true, t.can_untransform());
 
-        // Empty string: ""
-        let t = Transformation::FromBase32NoPadding;
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-        let original = t.untransform(result)?;
-        assert_eq!(b(b""), original);
-
         // Short string: "\x00"
         let t = Transformation::FromBase32NoPadding;
         let result = t.transform(b(b"IE"))?;
@@ -765,13 +924,6 @@ mod tests {
     fn test_base32_crockford() -> SimpleResult<()> {
         let t = Transformation::FromBase32Crockford;
         assert_eq!(true, t.can_untransform());
-
-        // Empty string: ""
-        let t = Transformation::FromBase32Crockford;
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-        let original = t.untransform(result)?;
-        assert_eq!(b(b""), original);
 
         // Short string: "\x00"
         let t = Transformation::FromBase32Crockford;
@@ -817,11 +969,6 @@ mod tests {
         let t = Transformation::FromBase32Permissive;
         assert_eq!(false, t.can_untransform());
 
-        // Empty string: ""
-        let t = Transformation::FromBase32Permissive;
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
-
         // Short string: "\x00"
         let t = Transformation::FromBase32Permissive;
         let result = t.transform(b(b"IE======"))?;
@@ -860,11 +1007,6 @@ mod tests {
     fn test_base32_crockford_permissive() -> SimpleResult<()> {
         let t = Transformation::FromBase32CrockfordPermissive;
         assert_eq!(false, t.can_untransform());
-
-        // Empty string: ""
-        let t = Transformation::FromBase32CrockfordPermissive;
-        let result = t.transform(b(b""))?;
-        assert_eq!(b(b""), result);
 
         // Short string: "\x00"
         let t = Transformation::FromBase32CrockfordPermissive;
